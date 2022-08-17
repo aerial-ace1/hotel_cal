27541ee246765fa9830ce98518df6073a179e939
aerial-ace1 <96421655+aerial-ace1@users.noreply.github.com>
2022-08-17 23:12:26 +0530

    done kinda

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-const landing = document.querySelector("#landing")
+const landing = document.querySelector("#landing");
 
 document.addEventListener("DOMContentLoaded", () => {
-    dish("pizza");
-    /* landing.width = screen.width
-    landing.height = screen.height/3 */
-})
+  const pics = document.querySelectorAll(".dishimg");
+  arra = ["pasta", "pizza", "Escovitch Fish", "Cajun spiced fish tacos"];
+  for (let i = 0; i < arra.length; i++) {
+    dish(arra[i], document.querySelector(`#img${i + 1}`));
+  }
 
-function dish(name){
-    const request = new XMLHttpRequest();
-    request.open("GET", `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
-    request.onload = () => {
-      const val = JSON.parse(request.responseText);
-      mealName = val.meals[0].strMeal
-      mealCuisine = val.meals[0].strArea
-      mealImg = val.meals[0].strMealThumb
-      document.querySelector("#landing").src = mealImg
-      document.querySelectorAll(".dishimg").forEach(pic => pic.src = mealImg);
-      
-    };
-    const data = new FormData();
-    request.send(data);
+  /* landing.width = screen.width
+    landing.height = screen.height/3 */
+});
+
+function dish(name, img) {
+  const request = new XMLHttpRequest();
+  request.open(
+    "GET",
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+  );
+  request.onload = () => {
+    const val = JSON.parse(request.responseText);
+    mealName = val.meals[0].strMeal;
+    mealCuisine = val.meals[0].strArea;
+    mealImg = val.meals[0].strMealThumb;
+    img.src = mealImg;
+  };
+  const data = new FormData();
+  request.send(data);
 }
